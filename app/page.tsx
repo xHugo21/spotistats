242ea0600dc063e1ef8c0d08b9032208c6a7de70
Hugo <hugo2121h@gmail.com>
2024-02-18 12:39:11 +0100

    feat: added dropdown menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,35 @@
-import { Button } from "../components/ui/button";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+let timeInterval: string = "4w"; // TODO: manage on value change DropdownRadioGroup
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Click here</Button>
+    <main className="p-24">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline">Time Interval</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuLabel>Select One</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuRadioGroup value={timeInterval}>
+            <DropdownMenuRadioItem value="4w">4 weeks</DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value="6m">6 months</DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value="at">All Time</DropdownMenuRadioItem>
+          </DropdownMenuRadioGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </main>
   );
 }
