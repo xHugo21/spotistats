cfec54328362d41ec2d6a5d960d12598161d22cb
xHugo21 <hugo2121h@gmail.com>
2024-12-26 18:31:16 +0100

    refactor: use client id from .env and gh secrets

--- lib/config.ts
+++ lib/config.ts
@@ -1,7 +1,7 @@
 export const BASE_PATH = "/spotistats";
 export const ITEMS_PER_PAGE = 20;
-export const SPOTIFY_CLIENT_ID = "_REMOVED";
+export const SPOTIFY_CLIENT_ID = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID;
 export const SPOTIFY_REDIRECT_URI =
   process.env.NODE_ENV === "development"
-    ? "http://localhost:3000/spotistats/callback"
-    : "https://xhugo21.github.io/spotistats/callback";
+    ? `http://localhost:3000${BASE_PATH}/callback`
+    : `https://xhugo21.github.io${BASE_PATH}/callback`;
